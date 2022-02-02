d7ac4e5e7e2fb71d86b02bc92a2fd2d462347f17
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-02 14:19:20 +0900

    added yarn + first part of code

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('results', event => {
+  console.log(event);
+});
